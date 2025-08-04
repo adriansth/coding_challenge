9ce18d2cce67e45577eaffaf2f620d223fd2604a
adriansth <soto.herrera.adrian@gmail.com>
2025-08-04 03:17:04 -0600

    refactor: function annotation

--- website/src/utils/strings.ts
+++ website/src/utils/strings.ts
@@ -1,3 +1,9 @@
+/**
+ * Truncate text to certain amount of characters, then add "..."
+ * @param text
+ * @param maxLength
+ * @returns
+ */
 export const truncateText = (text: string, maxLength: number = 500) => {
    if (!text) return;
    return text.length > maxLength ? text.substring(0, maxLength) + "..." : text;
